09183b3d88e3a3e3ab4b01f440e1b9b65e0191db
l_sawaniewski <l.sawaniewski@gmail.com>
2020-05-01 23:16:54 +0200

    general cleanup

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "please press 'q' for exit"
-python video_grabber_test.py 2>/dev/null
---
+++ src/data/video/video_sources.py
@@ -0,0 +1,3 @@
+info_channels = {
+    "tvp_info": "https://sdt-epix9-56.tvp.pl/token/video/live/14812849/20200501/1369639068/67c6d69f-ff7b-45e8-a914-706d084652e9/stream_0.m3u8",
+}
