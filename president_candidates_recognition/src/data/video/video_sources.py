f6837465b91d306ccb40512c3cb07ab6f8018d3d
l_sawaniewski <l.sawaniewski@gmail.com>
2020-05-16 16:53:10 +0200

    add environment yaml

--- president_candidates_recognition/src/data/video/video_sources.py
+++ president_candidates_recognition/src/data/video/video_sources.py
@@ -1,3 +1,3 @@
 info_channels = {
-    "tvp_info": "https://sdt-epix9-56.tvp.pl/token/video/live/14812849/20200516/1369639068/f38e5d79-dcb5-4845-912e-c9cc803430e8/stream_0.m3u8",
+    "tvp_info": "https://rsdt-kat201-67.tvp.pl/token/video/live/47747178/20200516/1369639068/9b8a9d4d-45df-44d4-bb32-3bea2bff6b63/stream_0.m3u8"
 }
